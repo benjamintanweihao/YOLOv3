431df52c50e5a1de08b0538623cb389366523872
Benjamin Tan <benjamin.tan@easymile.com>
2018-09-24 18:31:45 +0800

    Fleshed out YOLO layer, but haven't tested it out yet.
    TODO: Test out with image
    
    Eager execution allows the output of the YOLO head to display the
    tensors. However, I haven't figured out how to make the YOLO layer
    as part of the output to `keras.Model`.
    
    A workaround is to channel the outputs into the YOLO layer, though
    this solution is not very clean.

--- darknet.py
+++ darknet.py
@@ -1,16 +1,19 @@
 import os
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras import Input, Model
 from tensorflow.keras.layers import Add, BatchNormalization, Concatenate, Conv2D, \
     LeakyReLU, UpSampling2D
 from tensorflow.keras.regularizers import l2
 from tensorflow.python.keras.layers import ZeroPadding2D
 from tensorflow.keras.utils import plot_model as plot
+import tensorflow as tf
 import tensorflow.keras.backend as K
+import cv2
 
 from yolo_layer import YOLOLayer
 
+# NOTE: This lets us see the values from YOLO layers
+tf.enable_eager_execution()
 
 # NOTE: The original Darknet parser is at
 # NOTE: https://github.com/pjreddie/darknet/blob/master/src/parser.c
@@ -29,11 +32,11 @@ def darknet_base(inputs):
     Builds Darknet53 by reading the YOLO configuration file
 
     :param inputs: Input tensor
-    :return: A list of output layers and a ptr to the weights file
+    :return: A list of output layers, yolo_layers, and a ptr to the weights file
     """
     path = os.path.join(os.getcwd(), 'cfg', 'yolov3.cfg')
     blocks = parse_cfg(path)
-    x, layers, outputs = inputs, [], []
+    x, layers, yolo_layers = inputs, [], []
     ptr = 0
 
     for block in blocks:
@@ -43,40 +46,37 @@ def darknet_base(inputs):
             pass
 
         elif block_type == 'convolutional':
-            x, layers, outputs, ptr = _build_conv_layer(x, block, layers, outputs, ptr)
+            x, layers, yolo_layers, ptr = _build_conv_layer(x, block, layers, yolo_layers, ptr)
 
         elif block_type == 'shortcut':
-            x, layers, outputs, ptr = _build_shortcut_layer(x, block, layers, outputs, ptr)
+            x, layers, yolo_layers, ptr = _build_shortcut_layer(x, block, layers, yolo_layers, ptr)
 
         elif block_type == 'yolo':
-            x, layers, outputs, ptr = _build_yolo_layer(x, block, layers, outputs, ptr)
+            x, layers, yolo_layers, ptr = _build_yolo_layer(x, block, layers, yolo_layers, ptr)
 
         elif block_type == 'route':
-            x, layers, outputs, ptr = _build_route_layer(x, block, layers, outputs, ptr)
+            x, layers, yolo_layers, ptr = _build_route_layer(x, block, layers, yolo_layers, ptr)
 
         elif block_type == 'upsample':
-            x, layers, outputs, ptr = _build_upsample_layer(x, block, layers, outputs, ptr)
+            x, layers, yolo_layers, ptr = _build_upsample_layer(x, block, layers, yolo_layers, ptr)
 
         else:
             raise ValueError('{} not recognized as block type'.format(block_type))
 
     # NOTE: All the indices with NONE are YOLO layers. Therefore, the layer right
     # NOTE: before the YOLO layer is an output layer, which we are interested in.
-    # outputs = [layers[i - 1] for i in range(len(layers)) if layers[i] is None]
+    output_layers = [layers[i - 1] for i in range(len(layers)) if layers[i] is None]
 
-    return outputs, ptr
+    return output_layers, yolo_layers, ptr
 
 
 def _build_conv_layer(x, block, layers, outputs, ptr):
     stride = int(block['stride'])
-
-    # TODO: Not too sure how to change the input to add padding
-    pad = int(block['pad'])
-
     filters = int(block['filters'])
     kernel_size = int(block['size'])
+    pad = int(block['pad'])
     padding = 'same' if pad == 1 and stride == 1 else 'valid'
-    batch_normalize = 'batch_normalize' in block
+    use_batch_normalization = 'batch_normalize' in block
 
     # Darknet serializes convolutional weights as:
     # [bias/beta, [gamma, mean, variance], conv_weights]
@@ -95,7 +95,7 @@ def _build_conv_layer(x, block, layers, outputs, ptr):
     ptr += filters
 
     bn_weights_list = []
-    if batch_normalize:
+    if use_batch_normalization:
         # [gamma, mean, variance] * filters * 4 bytes
         bn_weights = np.ndarray(
             shape=(3, filters),
@@ -122,7 +122,7 @@ def _build_conv_layer(x, block, layers, outputs, ptr):
     # We would like to set these to Tensorflow order:
     # (height, width, in_dim, out_dim)
     conv_weights = np.transpose(conv_weights, [2, 3, 1, 0])
-    if batch_normalize:
+    if use_batch_normalization:
         conv_weights = [conv_weights]
     else:
         conv_weights = [conv_weights, conv_bias]
@@ -134,12 +134,12 @@ def _build_conv_layer(x, block, layers, outputs, ptr):
                kernel_size=kernel_size,
                strides=(stride, stride),
                padding=padding,
-               use_bias=not batch_normalize,
+               use_bias=not use_batch_normalization,
                activation='linear',
                kernel_regularizer=l2(5e-4),
                weights=conv_weights)(x)
 
-    if batch_normalize:
+    if use_batch_normalization:
         x = BatchNormalization(weights=bn_weights_list)(x)
 
     assert block['activation'] in ['linear', 'leaky'], 'Invalid activation: {}'.format(block['activation'])
@@ -196,11 +196,12 @@ def _build_yolo_layer(x, block, layers, outputs, ptr):
     # Anchors used based on mask indices
     anchors = [a for a in block['anchors'].split(',  ')]
     anchors = [anchors[i] for i in range(len(anchors)) if i in masks]
-    anchors = [tuple(a.split(',')) for a in anchors]
+    anchors = [tuple([int(a) for a in anchor.split(',')]) for anchor in anchors]
     classes = int(block['classes'])
 
-    x = YOLOLayer(classes=classes, anchors=anchors)(x)
-    outputs.append(x)
+    # x = YOLOLayer(num_classes=classes, anchors=anchors)(x)
+    yolo = YOLOLayer(num_classes=classes, anchors=anchors)
+    outputs.append(yolo)
     # NOTE: Here we append None to specify that the preceding layer is a output layer
     layers.append(None)
 
@@ -230,18 +231,13 @@ def parse_cfg(path):
         return blocks
 
 
-inputs = Input(shape=(416, 416, 3))
-outputs, weights_ptr = darknet_base(inputs)
+# TODO: Don't hard code the image height and width
+inputs = Input(shape=(608, 608, 3))
+output_layers, yolo_layers, weights_ptr = darknet_base(inputs)
 
-model = Model(inputs, outputs)
-model.summary()
-
-# for output in outputs:
-#     x = output[-1]
-#     x = YOLOLayer(output_dim=(x.get_shape()[0], -1, 3, 85))(x)
-#     print(x.get_shape())
-
-plot(model, to_file='utils/model.png', show_shapes=True)
+######################
+# Check weights file #
+######################
 
 # Check that the weights file has been completely consumed.
 
@@ -253,3 +249,45 @@ if remaining_weights > 0:
     print('Warning: {} unused weights'.format(remaining_weights))
 else:
     print('Weights loaded successfully!')
+
+
+model = Model(inputs, output_layers)
+model.summary()
+
+plot(model, to_file='utils/model.png', show_shapes=True)
+
+# Feed in one image
+
+orig = cv2.imread('utils/dog-cycle-car.png')
+# TODO: Don't hard code the image height and width
+orig = cv2.resize(orig, (608, 608))  # Resize to the input dimension
+
+img = orig.astype(np.float32)
+img = img[:, :, ::-1]  # BGR -> RGB
+img /= 255.0
+img = np.expand_dims(img, axis=0)
+
+predictions = model.predict([img])
+
+for yolo_layer, prediction in zip(yolo_layers, predictions):
+    # NOTE: The values can be seen due to eager mode.
+    box_xy, box_wh, objectness, class_scores = yolo_layer(prediction)
+
+    box_xy = box_xy[0]
+    box_wh = box_wh[0]
+    objectness = objectness[0]
+    class_scores = class_scores[0]
+
+    for (x1, y1), (w, h) in zip(box_xy, box_wh):
+        x2 = x1 + w
+        y2 = y1 + h
+
+        if max([x1, x2, y1, y2]) <= 608:
+            cv2.rectangle(orig, (x1, y1), (x2, y2), (255, 0, 0), 1)
+
+    cv2.imwrite("out.png", orig)
+
+    # print(box_xy)
+    # print(box_wh)
+    # print(objectness)
+    # print(class_scores)
--- yolo_layer.py
+++ yolo_layer.py
@@ -3,28 +3,75 @@ import tensorflow as tf
 
 class YOLOLayer(tf.keras.layers.Layer):
 
-    def __init__(self, classes, anchors, **kwargs):
-        self.classes = classes
+    def __init__(self, num_classes, anchors, **kwargs):
+        self.num_classes = num_classes
         self.anchors = anchors
+
+        # TODO: Remove this hardcoded value later.
+        self.input_image_dim = (608, 608)
+
         super(YOLOLayer, self).__init__(**kwargs)
 
-    def build(self, input_shape):
-        super(YOLOLayer, self).build(input_shape)
+    def call(self, prediction, **kwargs):
+        batch_size = tf.shape(prediction)[0]
+        stride = self.input_image_dim[0] // tf.shape(prediction)[1]
+        grid_size = self.input_image_dim[0] // stride
+        num_anchors = len(self.anchors)
 
-    def call(self, inputs, **kwargs):
-        outputs = tf.reshape(inputs, shape=(-1, -1, 3, self.classes + 5))
+        prediction = tf.reshape(prediction,
+                                shape=(batch_size, self.num_classes + 5, num_anchors * grid_size * grid_size))
+        prediction = tf.transpose(prediction, [0, 2, 1])
+        prediction = tf.reshape(prediction,
+                                shape=(batch_size, num_anchors * grid_size * grid_size, self.num_classes + 5))
 
-        return outputs
+        box_xy = tf.sigmoid(prediction[:, :, :2])  # t_x (box x and y coordinates)
+        objectness = tf.sigmoid(prediction[:, :, 4])  # p_o (objectness score)
+
+        grid = tf.range(grid_size)
+        a, b = tf.meshgrid(grid, grid)
+
+        x_offset = tf.reshape(a, (-1, 1))
+        y_offset = tf.reshape(b, (-1, 1))
+
+        x_y_offset = tf.concat((x_offset, y_offset), axis=1)
+        x_y_offset = tf.tile(x_y_offset, (1, num_anchors))
+        x_y_offset = tf.reshape(x_y_offset, (-1, 2))
+        x_y_offset = tf.expand_dims(x_y_offset, 0)
+        x_y_offset = tf.cast(x_y_offset, dtype='float32')
+
+        # Note the `:2` (adding x, y coordinates together)
+        box_xy += x_y_offset
+
+        # Log space transform of the height and width
+        # anchors = tf.constant(anchors, dtype='float32')
+        anchors = tf.cast([(a[0] / stride, a[1] / stride) for a in self.anchors], dtype='float32')
+        anchors = tf.tile(anchors, (grid_size * grid_size, 1))
+        anchors = tf.expand_dims(anchors, 0)
+
+        box_wh = tf.exp(prediction[:, :, 2:4]) * anchors
+
+        # Sigmoid class scores
+        class_scores = tf.sigmoid(prediction[:, :, 5:])
+
+        # Resize detection map back to the input image size
+        stride = tf.cast(stride, dtype='float32')
+        box_xy *= stride
+        box_wh *= stride
+
+        # box_xy = tf.Print(box_xy, [box_xy], message='Box XY')
+        # box_wh = tf.Print(box_wh, [box_wh], message='Box WH')
+        # objectness = tf.Print(objectness, [objectness], message='Objectness')
+        # class_scores = tf.Print(class_scores, [class_scores], message='Class Scores')
+
+        return box_xy, box_wh, objectness, class_scores
 
     def compute_output_shape(self, input_shape):
-        input_shape = tf.reshape(input_shape,
-                                 shape=(input_shape[0], -1, 3, self.classes + 5))
+        num_anchors = len(self.anchors)
 
-        return input_shape.get_shape()
+        n, h, w, c = input_shape
+        box_xy = (None, h, w, num_anchors, 2)
+        box_wh = (None, h, w, num_anchors, 2)
+        box_confidence = (None, h, w, num_anchors, 1)
+        box_class_probs = (None, h, w, num_anchors, self.num_classes)  # TODO: NOT TOO SURE ABOUT THIS
 
-    def get_config(self):
-        return super(YOLOLayer, self).get_config()
-
-    @classmethod
-    def from_config(cls, config):
-        return cls(**config)
+        return box_xy, box_wh, box_confidence, box_class_probs
